ddd26b22be63c7a4c4844483c7ebcd6acd1a3ce1
jeongseohan93 <minsang580@gmail.com>
2025-06-06 16:24:20 +0900

    feat: 회원가입 기능 구현 완료

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,28 +3,37 @@ const Sequelize = require('sequelize');
 class User extends Sequelize.Model {
     static initiate(sequelize) {
          User.init({
-            email: {
-                type: Sequelize.STRING(40),
-                allowNull: true,
-                unique: true,
-            },
-            nick: {
-                type: Sequelize.STRING(15),
+            user_uuid: {
+                type: Sequelize.CHAR(36),
+                defaultValue: Sequelize.UUIDV4,
                 allowNull: false,
+                primaryKey: true
+            },
+            email: {
+            type: Sequelize.STRING(100),
+            allowNull: true
             },
             password: {
+                type: Sequelize.STRING(255),
+                allowNull: false
+            },
+            name: {
                 type: Sequelize.STRING(100),
-                allowNull: true,
+                allowNull: true
             },
-            provider: {
-                type: Sequelize.ENUM('local', 'kakao'),
+            phone: {
+                type: Sequelize.STRING(20),
+                allowNull: true
+            },
+            address: {
+                type: Sequelize.STRING(255),
+                allowNull: true
+            },
+            role: {
+                type: Sequelize.ENUM('admin','user'),
                 allowNull: false,
-                defaultValue: 'local'
-            },
-            snsId: {
-                type: Sequelize.STRING(30),
-                allowNull: true,
-            },
+                defaultValue: "user"
+            }
         }, {
             sequelize,
             timestamps: true, // createdAt, updatedAt
@@ -34,7 +43,22 @@ class User extends Sequelize.Model {
             paranoid: true, //deletedAt 유저 삭제일 // soft delete
             charset: 'utf8',
             collate: 'utf8_general_ci',
-        })
+
+            indexes: [
+                {
+                    name: "PRIMARY",
+                    unique: true,
+                    using: "BTREE",
+                    fields: [{ name: "user_uuid" }]
+                },
+                {
+                    name: "email",
+                    unique: true,
+                    using: "BTREE",
+                    fields: [{ name: "email" }]
+                }
+            ]
+        });
     }
 }
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
-
+import Register from "./pages/Register.jsx";
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<Register/>}/>
       </Routes>
     </BrowserRouter>
   );
